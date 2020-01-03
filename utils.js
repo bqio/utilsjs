02763c7989daaf8dc125c25bbba53bd080e83fd5
Dmitry Mikhaltsov <45086516+bqio@users.noreply.github.com>
2020-01-03 23:40:32 +0400

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,52 @@
+function fetchImage(src) {
+  return new Promise((resolve, reject) => {
+    const image = new Image()
+    image.onerror = () => reject('Некорректное изображение.')
+    image.onload = () => resolve(image)
+    image.src = src
+  })
+}
+
+function fetchText(src) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.onload = () => resolve(xhr.responseText)
+    xhr.onprogress = (e) => {
+      if (e.lengthComputable) {
+        console.log(`Загрузка... (${src}) ${b2Kb(e.loaded).toFixed(1)} KB / ${b2Kb(e.total).toFixed(1)} KB`)
+      } else {
+        console.log(`Загрузка... (${src}) ${b2Kb(e.loaded).toFixed(1)} KB`)
+      }
+    }
+    xhr.onerror = () => reject('Ошибка соединения.')
+    xhr.open('GET', src, true)
+    xhr.send(null)
+  })
+}
+
+function fetchJson(src) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.onload = () => {
+      try {
+        resolve(JSON.parse(xhr.responseText))
+      } catch (e) {
+        reject('Некорректный JSON.')
+      }
+    }
+    xhr.onprogress = (e) => {
+      if (e.lengthComputable) {
+        console.log(`Загрузка... (${src}) ${b2Kb(e.loaded).toFixed(1)} KB / ${b2Kb(e.total).toFixed(1)} KB`)
+      } else {
+        console.log(`Загрузка... (${src}) ${b2Kb(e.loaded).toFixed(1)} KB`)
+      }
+    }
+    xhr.onerror = () => reject('Ошибка соединения.')
+    xhr.open('GET', src, true)
+    xhr.send(null)
+  })
+}
+
+function b2Kb(b) {
+  return b / 1000
+}
